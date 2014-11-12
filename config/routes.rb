f749b423d9a92df662673fb0c7474f2d53362bc0
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-11-12 23:18:32 +0900

    groupsの詳細ページをindexではなくshowで表示するように変更

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit]
   resources :groups, only: [:index, :show, :new, :edit]
   get "games/search" => "games#search"
-  resources :games, only: [:index, :show, :new, :edit]
+  resources :games, only: [:show, :new, :edit]
 
   namespace :admin do
     resources :users, only: %i(index edit new)
