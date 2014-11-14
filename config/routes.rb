5a477a1a396c2e86094feb23c001b0ea3688f7da
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-11-14 19:31:10 +0900

    routesからgamesのindexを追加、groupsのindexを削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   resource :top, only: [:index]
   resource :login
   resources :users, only: [:index, :edit]
-  resources :groups, only: [:index, :show, :new, :edit]
+  resources :groups, only: [:show, :new, :edit]
   get "games/search" => "games#search"
-  resources :games, only: [:show, :new, :edit]
+  resources :games, only: [:index, :show, :new, :edit]
 
   namespace :admin do
     resources :users, only: %i(index edit new)
