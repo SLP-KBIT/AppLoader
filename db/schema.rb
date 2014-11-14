aea2b3296ffb1a1e445c60ac5e92e4ee2637f418
hankyu-yatsu <ssymsh28+Git@gmail.com>
2014-11-14 21:55:45 +0900

    Userのmodelを作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141114113630) do
+ActiveRecord::Schema.define(version: 20141114125209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,17 @@ ActiveRecord::Schema.define(version: 20141114113630) do
 
   create_table "tags", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "account"
+    t.string   "name"
+    t.string   "password_digest"
+    t.boolean  "enable_flag"
+    t.boolean  "admin_flag"
+    t.string   "icon"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :user do
+    account "MyString"
+name "MyString"
+password_digest "MyString"
+enable_flag false
+admin_flag false
+icon "MyString"
+  end
+
+end
