ef4ea07d65a562b380585637d8578a774e2e09a2
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-12-12 20:53:26 +0900

    seedで、gameを30個生成するように修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
+
 engineer = User.create(name:"Engineer", account:"engineer", password_digest:"hoge")
 
 group = engineer.groups.create(name:"engineer", summary:"hoge")
 
 game = group.games.create(title:"眠れるラボのなおすけ", summary:"naosuke", version:"1.0", game_file:"dammy", format:"web", icon:"dammy")
-
 game.tags.create(name:"富永研究室")
+
+(1..30).to_a.each do |i|
+  game = group.games.create(title:"Game#{i}", summary:"naosuke", version:"1.0", game_file:"dammy", format:"web", icon:"dammy")
+  game.tags.create(name:"富永研究室")
+end
