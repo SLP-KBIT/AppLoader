062e7cb3f31b10883c111e84044389a8b5f9e59e
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2015-09-24 15:22:00 +0900

    Add Game spec description

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -20,4 +20,17 @@ require 'rails_helper'
 
 RSpec.describe Game, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  describe Game do
+    it "is valid with a title, icon, group_id, summary, version, game_file, dl_count, format"
+    it "is invalid without a title"
+    it "is invalid without a icon"
+    it "is invalid without a group_id"
+    it "is invalid without a summary"
+    it "is invalid without a version"
+    it "is invalid without a game_file"
+    it "is invalid without a dl_count"
+    it "is invalid without a format"
+    it "is invalid with a duplicate title"
+    it "is invalid without a included group_id in Group"
+  end
 end
