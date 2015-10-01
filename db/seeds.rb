1793d647d11a7d4539aa5faa62eefc067edd5b13
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2015-10-01 15:40:23 +0900

    Add default Game's icon

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,10 @@ engineer = User.create(name:"Engineer", account:"engineer", password_digest:"hog
 
 group = engineer.groups.create(name:"engineer", summary:"hoge")
 
-game = group.games.create(title:"眠れるラボのなおすけ", summary:"naosuke", version:"1.0", game_file:"dammy", format:"web", icon:"dammy")
+game = group.games.create(title:"眠れるラボのなおすけ", summary:"naosuke", version:"1.0", game_file:"sample.exe", format:"web", icon:"sample.png")
 game.tags.create(name:"富永研究室")
 
 (1..30).to_a.each do |i|
-  game = group.games.create(title:"Game#{i}", summary:"naosuke", version:"1.0", game_file:"dammy", format:"web", icon:"dammy")
+  game = group.games.create(title:"Game#{i}", summary:"naosuke", version:"1.0", game_file:"sample.exe", format:"web", icon:"sample.png")
   game.tags.create(name:"富永研究室")
 end
