9d01593c50c1a5b3cca99bf6a2f5620c829b6d38
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2015-10-02 11:11:35 +0900

    create test for top/index

--- spec/controllers/top_controller_spec.rb
+++ spec/controllers/top_controller_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe TopController, type: :controller do
+  describe "GET '/index'" do
+    it 'should render a index template' do
+      get :index
+      expect(response).to render_template :index
+    end
+
+    it 'should load a recent Games' do
+      get :index
+      expect(assigns[:dl_games]).to eq Game.dl_top
+    end
+
+    it 'should load a popular Games' do
+      get :index
+      expect(assigns[:new_games]).to eq Game.new_top
+    end
+  end
 end
