8c9c6c768d656c684299b8898c13cdb530d9338a
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2015-10-21 14:31:44 +0900

    fix Tag Model than many let put together

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -11,22 +11,23 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
+  let(:tag) { build(:tag) }
+
   context 'when has necessary parameters' do
-    let(:tag) { build(:tag) }
     it { expect(tag).to be_valid }
   end
 
   context 'when name is nil' do
-    let(:tag) { build(:tag, name: nil) }
     it do
+      tag.name = nil
       tag.valid?
       expect(tag.errors[:name]).to include("can't be blank")
     end
   end
 
   context 'when has a duplicate name' do
-    let(:tag) { create(:tag) }
     it do
+      tag.save
       duplicate_tag = Tag.new(name: tag.name)
       duplicate_tag.valid?
       expect(duplicate_tag.errors[:name]).to include('has already been taken')
