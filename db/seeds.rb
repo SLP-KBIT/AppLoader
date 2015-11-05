354b6c4b60a31bfa52745aee4200cad171154d86
joniyjoniy <s13t264@stmail.eng.kagawa-u.ac.jp>
2015-11-06 02:05:10 +0900

    fix game's format in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
-
 engineer = User.create(name: 'Engineer', account: 'engineer', password_digest: 'hoge', icon: 'hoge.png')
 group = engineer.groups.create(name: 'engineer', summary: 'hoge', icon: 'icon.png')
 
 tag = Tag.create(name: '富永研究室')
 
-game = group.games.create(title: '眠れるラボのなおすけ', summary: 'naosuke', version: '1.0', game_file: 'sample.exe', format: 'web', icon: 'sample.png')
+game = group.games.create(title: '眠れるラボのなおすけ', summary: 'naosuke', version: '1.0', game_file: 'sample.exe', format: 'PC', icon: 'sample.png')
 tag.games << game
 
 (1..30).to_a.each do |i|
-  game = group.games.create(title: "Game#{i}", summary: 'naosuke', version: '1.0', game_file: 'sample.exe', format: 'web', icon: 'sample.png')
+  game = group.games.create(title: "Game#{i}", summary: 'naosuke', version: '1.0', game_file: 'sample.exe', format: 'PC', icon: 'sample.png')
   tag.games << game
 end
