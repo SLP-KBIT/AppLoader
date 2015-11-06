7ffef15aae54d65c475ddf58e8942fa12574e87e
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2015-11-07 06:21:12 +0900

    Receive game file

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -14,4 +14,41 @@ class GamesController < ApplicationController
 
   def show
   end
+
+  def create
+    tmp_param = game_params
+    tmp_param['game_file'] = tmp_param['game_file'].original_filename
+    tmp_param['icon'] = tmp_param['icon'].original_filename
+    tmp_param['movie'] = tmp_param['movie'].original_filename
+    game = Game.new(tmp_param)
+    game.save
+    gp = game_params
+    game_upload(gp['game_file'], game)
+    redirect_to games_path
+  end
+
+  private
+
+  def game_upload(file = file, game = game)
+    path = "#{DATA_PATH[:game]}/#{game.id}"
+    file_upload(file, game, path)
+  end
+
+  def file_upload(file = file, game = game, path = path)
+    link_name = Digest::SHA1.hexdigest(game.title)
+    FileUtils.mkdir_p path unless FileTest.exist? path
+    File.open("#{path}/#{file.original_filename}", 'wb') { |f| f.write(file.read) }
+  end
+
+  def game_params
+    params.require(:game).permit(:game_file,
+                                 :title,
+                                 :format,
+                                 :title,
+                                 :version,
+                                 :icon,
+                                 :group_id,
+                                 :summary,
+                                 :movie)
+  end
 end
---
+++ config/initializers/constants.rb
@@ -0,0 +1,6 @@
+DATA_PATH = { game: Rails.root.to_s + '/data/game',
+              movie: Rails.root.to_s + '/data/movie',
+              icon: Rails.root.to_s + '/data/icon' }
+LINK_PATH = { game: Rails.root.to_s + '/public/game',
+              movie: Rails.root.to_s + '/public/movie',
+              icon: Rails.root.to_s + '/public/icon' }
