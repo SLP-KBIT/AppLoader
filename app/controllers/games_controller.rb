c8a0d15751fffcd5ef632e421b47bbea4ce4fcd6
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2015-11-09 13:00:15 +0900

    Refoctoring for rubocop

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -24,6 +24,8 @@ class GamesController < ApplicationController
     game.save
     gp = game_params
     game_upload(gp['game_file'], game)
+    icon_upload(gp['icon'], game)
+    movie_upload(gp['movie'], game)
     redirect_to games_path
   end
 
@@ -31,10 +33,23 @@ class GamesController < ApplicationController
 
   def game_upload(file = file, game = game)
     path = "#{DATA_PATH[:game]}/#{game.id}"
-    file_upload(file, game, path)
+    link_path = LINK_PATH[:game]
+    file_upload(file, game, path, link_path)
   end
 
-  def file_upload(file = file, game = game, path = path)
+  def icon_upload(file = file, game = game)
+    path = "#{DATA_PATH[:icon]}/#{game.id}"
+    link_path = LINK_PATH[:icon]
+    file_upload(file, game, path, link_path)
+  end
+
+  def movie_upload(file = file, game = game)
+    path = "#{DATA_PATH[:movie]}/#{game.id}"
+    link_path = LINK_PATH[:movie]
+    file_upload(file, game, path, link_path)
+  end
+
+  def file_upload(file, game, path, link_path)
     link_name = Digest::SHA1.hexdigest(game.title)
     FileUtils.mkdir_p path unless FileTest.exist? path
     File.open("#{path}/#{file.original_filename}", 'wb') { |f| f.write(file.read) }
